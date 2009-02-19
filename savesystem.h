71153331e5285aaa2a4ba8d6e2e2c2636977b4d3
Artur Duque de Souza <asouza@kde.org>
2009-02-19 16:03:39 +0000

    skeleton for savesystem
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>
    
    svn path=/trunk/playground/base/plasma/plasmate/; revision=928547

---
+++ savesystem.h
@@ -0,0 +1,21 @@
+/*
+  Copyright (c) 2009 Riccardo Iaconelli <riccardo@kde.org>
+ 
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+ 
+#ifndef SAVESYSTEM_H
+#define SAVESYSTEM_H
+ 
+#include <QWidget>
+ 
+class SaveSystem : public QWidget
+{
+    public:
+        SaveSystem(QWidget *parent = 0);
+};
+ 
+#endif // TEXTEDITOR_H
