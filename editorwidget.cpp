5370454d320f8728c14d950c7f90cf95299145a4
Artur Duque de Souza <asouza@kde.org>
2009-02-19 15:53:21 +0000

    stub file for the editor widget
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>
    
    svn path=/trunk/playground/base/plasma/plasmate/; revision=928458

---
+++ editorwidget.cpp
@@ -0,0 +1,14 @@
+/*
+  Copyright (c) 2009 Riccardo Iaconelli <riccardo@kde.org>
+ 
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+ 
+#include "editorwidget.h"
+ 
+EditorWidget::EditorWidget()
+{
+}
