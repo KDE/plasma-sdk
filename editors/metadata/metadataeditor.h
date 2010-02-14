9a6a83ceffc49a63f6da12f4fa7e3a9b05ed062a
Richard J. Moore <rich@kde.org>
2010-02-14 12:55:41 +0000

    - Add copyright headers.
    svn path=/trunk/playground/base/plasma/plasmate/; revision=1090007

--- editors/metadata/metadataeditor.h
+++ editors/metadata/metadataeditor.h
@@ -1,3 +1,12 @@
+/*
+  Copyright (c) 2009 Richard Moore, <rich@kde.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
 #ifndef METADATAEDITOR_H
 #define METADATAEDITOR_H
 
