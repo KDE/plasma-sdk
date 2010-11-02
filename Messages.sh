aaa5f9bb1c921a45d4a3ea461918477c572bfab4
Pino Toscano <pino@kde.org>
2010-11-02 10:08:21 +0000

    do not extract messages from sources in 'test' subdirectories
    (also remove redundant shell expansions, already covered by the `find` invocation)
    
    svn path=/trunk/playground/base/plasma/plasmate/; revision=1192159

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find -name \*.ui -o -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find -name \*.cpp -o -name \*.h` *.cpp *.h -o $podir/plasmate.pot
+$XGETTEXT `find -name \*.cpp -o -name \*.h | grep -v '/test/'` -o $podir/plasmate.pot
 rm -f rc.cpp
