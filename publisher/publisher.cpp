4a8b353efb8b2ba09c5ff2c880c855b99a390852
Sebastian Kgler <sebas@kde.org>
2011-03-14 13:34:12 +0000

    use full copyright headers according to KDE's licensing policy. No license changes in this commit.
    svn path=/trunk/playground/base/plasma/plasmate/; revision=1224873

--- publisher/publisher.cpp
+++ publisher/publisher.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2009 Lim Yuen Hoe <yuenhoe@hotmail.com>
+  Copyright 2009 Lim Yuen Hoe <yuenhoe@hotmail.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
--- publisher/signingwidget.h
+++ publisher/signingwidget.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010 by Diego '[Po]lentino' Casella <polentino911@gmail.com>
+ *  Copyright 2010 by Diego '[Po]lentino' Casella <polentino911@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
