19e4a50cca4b4c33ad84c8e2acd624a386ec7fed
Sebastian Kgler <sebas@kde.org>
2011-03-14 13:34:08 +0000

    use full copyright headers according to KDE's licensing policy. No license changes in this commit.
    svn path=/trunk/playground/base/plasma/plasmate/; revision=1224872

--- editors/kconfigxt/kconfigxteditor.cpp
+++ editors/kconfigxt/kconfigxteditor.cpp
@@ -1,6 +1,6 @@
 /*
    This file is part of the KDE project
-   Copyright (C) 2009 by Dmitry Suzdalev <dimsuz@gmail.com>
+   Copyright 2009 by Dmitry Suzdalev <dimsuz@gmail.com>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
