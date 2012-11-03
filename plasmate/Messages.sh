1ba0b3ca3b0e34b491acd229f537dca9f5e86103
Pino Toscano <pino@kde.org>
2012-11-03 17:15:36 +0100

    i18n: skip stuff from the 'test-kpart' subdir

--- plasmate/Messages.sh
+++ plasmate/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find -name \*.ui -o -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find -name \*.cpp -o -name \*.h | grep -v '/test/'` -o $podir/plasmate.pot
+$EXTRACTRC `find -name \*.ui -o -name \*.rc | grep -v '/test-kpart/'` >> rc.cpp || exit 11
+$XGETTEXT `find -name \*.cpp -o -name \*.h | grep -v '/test/' | grep -v '/test-kpart/'` -o $podir/plasmate.pot
 rm -f rc.cpp
