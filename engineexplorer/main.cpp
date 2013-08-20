81ad8968eb633bb39b522dac0870ec0d51e2288b
Giorgos Tsiapaliokas <terietor@gmail.com>
2013-08-20 16:14:08 +0300

    don't allocate it in the heap

--- engineexplorer/main.cpp
+++ engineexplorer/main.cpp
@@ -76,25 +76,25 @@ int main(int argc, char **argv)
 
     QApplication app(argc, argv);
 
-    QCommandLineParser *parser = new QCommandLineParser;
+    QCommandLineParser parser;
     app.setApplicationVersion(version);
-    parser->addVersionOption();
-    parser->addHelpOption(description);
-    parser->addOption(QCommandLineOption(QStringList() << "list", i18n("Displays a list of known engines and their descriptions")));
-    parser->addOption(QCommandLineOption(QStringList() << "height", i18n("The desired height in pixels"), "pixels"));
-    parser->addOption(QCommandLineOption(QStringList() << "width", i18n("The desired width in pixels"), "pixels"));
-    parser->addOption(QCommandLineOption(QStringList() << "x", i18n("The desired x position in pixels"), "pixels"));
-    parser->addOption(QCommandLineOption(QStringList() << "y", i18n("The desired y position in pixels"), "pixels"));
-    parser->addOption(QCommandLineOption(QStringList() << "engine", i18n("The data engine to use"), "data engine"));
-    parser->addOption(QCommandLineOption(QStringList() << "source", i18n("The source to request"), "data engine"));
-    parser->addOption(QCommandLineOption(QStringList() << "interval", i18n("Update interval in milliseconds"), "ms"));
-    parser->addOption(QCommandLineOption(QStringList() << "app", i18n("Only show engines associated with the parent application; "
+    parser.addVersionOption();
+    parser.addHelpOption(description);
+    parser.addOption(QCommandLineOption(QStringList() << "list", i18n("Displays a list of known engines and their descriptions")));
+    parser.addOption(QCommandLineOption(QStringList() << "height", i18n("The desired height in pixels"), "pixels"));
+    parser.addOption(QCommandLineOption(QStringList() << "width", i18n("The desired width in pixels"), "pixels"));
+    parser.addOption(QCommandLineOption(QStringList() << "x", i18n("The desired x position in pixels"), "pixels"));
+    parser.addOption(QCommandLineOption(QStringList() << "y", i18n("The desired y position in pixels"), "pixels"));
+    parser.addOption(QCommandLineOption(QStringList() << "engine", i18n("The data engine to use"), "data engine"));
+    parser.addOption(QCommandLineOption(QStringList() << "source", i18n("The source to request"), "data engine"));
+    parser.addOption(QCommandLineOption(QStringList() << "interval", i18n("Update interval in milliseconds"), "ms"));
+    parser.addOption(QCommandLineOption(QStringList() << "app", i18n("Only show engines associated with the parent application; "
                                            "maps to the X-KDE-ParentApp entry in the DataEngine's .desktop file."), "application"));
 
 
-    parser->process(app);
+    parser.process(app);
 
-    if (parser->isSet("list")) {
+    if (parser.isSet("list")) {
         listEngines();
         return 0;
     }
@@ -103,38 +103,38 @@ int main(int argc, char **argv)
 
     bool ok1, ok2 = false;
     //get size
-    int x = parser->value("height").toInt(&ok1);
-    int y = parser->value("width").toInt(&ok2);
+    int x = parser.value("height").toInt(&ok1);
+    int y = parser.value("width").toInt(&ok2);
     if (ok1 && ok2) {
         w->resize(x,y);
     }
 
     //get pos if available
-    x = parser->value("x").toInt(&ok1);
-    y = parser->value("y").toInt(&ok2);
+    x = parser.value("x").toInt(&ok1);
+    y = parser.value("y").toInt(&ok2);
     if (ok1 && ok2) {
         w->move(x,y);
     }
 
     //set interval
-    int interval = parser->value("interval").toInt(&ok1);
+    int interval = parser.value("interval").toInt(&ok1);
     if (ok1) {
         w->setInterval(interval);
     }
 
     //set engine
-    QString engine = parser->value("engine");
+    QString engine = parser.value("engine");
     if (!engine.isEmpty()) {
         w->setEngine(engine);
 
-        QString source = parser->value("source");
+        QString source = parser.value("source");
         if (!source.isEmpty()) {
             w->requestSource(source);
         }
     }
 
-    if (parser->isSet("app")) {
-        w->setApp(parser->value("app"));
+    if (parser.isSet("app")) {
+        w->setApp(parser.value("app"));
     }
 
     w->show();
