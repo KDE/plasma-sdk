fcfb0ddcc63e4593e58b93ac68e151f156316819
Giorgos Tsiapaliokas <terietor@gmail.com>
2014-01-01 17:46:37 +0200

    adapt packagehandlertest into the new changes

--- plasmate/tests/packagehandlertest.cpp
+++ plasmate/tests/packagehandlertest.cpp
@@ -37,89 +37,154 @@ PackageHandlerTest::~PackageHandlerTest()
 void PackageHandlerTest::initTestCase()
 {
     QStandardPaths::enableTestMode(true);
-
     connect(&m_packageHandler, &PackageHandler::error, [=](const QString &error) {
         QFAIL(error.toLocal8Bit().data());
     });
+}
 
+void PackageHandlerTest::initPlasmoid()
+{
     m_packageHandler.setPackageType(QStringLiteral("Plasma/Applet"));
 }
 
-void PackageHandlerTest::checkDirectories()
+void PackageHandlerTest::createNewPlasmoid()
 {
-    // FIXME
-    m_packageHandler.setPackagePath(QStringLiteral("org.kde.tests.packagehandlertest"));
+    QString newPlasmoidLocation = QStandardPaths::standardLocations(QStandardPaths::DataLocation).at(0);
 
-    QDir dir(m_packageHandler.projectPath() + m_packageHandler.contentsPrefix());
+    QDir templatesDir(newPlasmoidLocation);
+    templatesDir.mkpath(QStringLiteral("templates"));
+    templatesDir.cd(QStringLiteral("templates"));
+
+    if (!newPlasmoidLocation.endsWith(QLatin1Char('/'))) {
+        newPlasmoidLocation += QLatin1Char('/') + QStringLiteral("org.kde.packagehandlertest.newPlasmoid");
+    }
+
+    const QString mainPlasmoidFile = QFINDTESTDATA("testdata/mainPlasmoid.qml");
+    Q_ASSERT(!mainPlasmoidFile.isEmpty());
+
+    QFile f(mainPlasmoidFile);
+    f.copy(templatesDir.path() + QLatin1Char('/') + QStringLiteral("mainPlasmoid.qml"));
+
+    m_packageHandler.setPackagePath(newPlasmoidLocation);
+    QDir dir(newPlasmoidLocation);
     Q_ASSERT(dir.exists());
-    //FIXME make it more generic?
+    Q_ASSERT(dir.exists(QStringLiteral("contents")));
+    Q_ASSERT(dir.cd(QStringLiteral("contents")));
+    Q_ASSERT(dir.exists(QStringLiteral("ui")));
+    Q_ASSERT(dir.exists(QStringLiteral("metadata.desktop")));
     Q_ASSERT(dir.cd(QStringLiteral("ui")));
-    QFile mainFile(dir.path() + QStringLiteral("/main.qml"));
-    Q_ASSERT(mainFile.exists());
+    Q_ASSERT(dir.exists(QStringLiteral("main.qml")));
 }
 
-void PackageHandlerTest::checkLoadPackageInfo()
+void PackageHandlerTest::loadPlasmoidTestData()
 {
-    QFile testFile(m_packageHandler.projectPath() + m_packageHandler.contentsPrefix() +
-                   QStringLiteral("ui/foo.qml"));
-    testFile.open(QIODevice::ReadWrite);
-    testFile.close();
+    const QString packagePath = QFINDTESTDATA("testdata/org.kde.tests.packagehandlertest");
+    Q_ASSERT(!packagePath.isEmpty());
 
-    testFile.setFileName(m_packageHandler.projectPath() + m_packageHandler.contentsPrefix() +
-                         QStringLiteral("bar.qml"));
-    testFile.open(QIODevice::ReadWrite);
-    testFile.close();
+    m_packageHandler.setPackagePath(packagePath);
+}
 
-    QList<PackageHandler::Node> nodes = m_packageHandler.loadPackageInfo();
-
-    Q_ASSERT_X(nodes.size() == 9, "Check node list size", QString(nodes.size()).toLocal8Bit().data());
-
-    QStringList topEntriesList;
-    topEntriesList.append(QStringLiteral("images"));
-    topEntriesList.append(QStringLiteral("scripts"));
-    topEntriesList.append(QStringLiteral("data"));
-    topEntriesList.append(QStringLiteral("ui"));
-    topEntriesList.append(QStringLiteral("theme"));
-    topEntriesList.append(QStringLiteral("config"));
-    topEntriesList.append(QStringLiteral("translations"));
-    topEntriesList.append(QStringLiteral("bar.qml"));
-
-    QStringList indexedTopLevel;
-
-    bool loopOk = false;
-
-    for (const auto &it : nodes) {
-        if (it.name != QStringLiteral("metadata.desktop")) {
-            Q_ASSERT_X(topEntriesList.contains(it.name),
-                       "checking top entries node", it.name.toLocal8Bit().data());
-
-            indexedTopLevel.append(it.name);
-
-            if (it.name == QStringLiteral("ui")) {
-                QStringList uiDirFileList;
-                for (const auto &uiFile : it.children) {
-                    uiDirFileList.append(uiFile.name);
-                }
-
-                loopOk = !uiDirFileList.isEmpty();
-
-                const QString path= m_packageHandler.projectPath() +
-                                    m_packageHandler.contentsPrefix() + it.name;
-                Q_ASSERT_X(uiDirFileList.contains(QStringLiteral("foo.qml")),
-                           "Checking unamed files under named directory",
-                           path.toLocal8Bit().data());
+void PackageHandlerTest::checkPlasmoidMimeTypes()
+{
+    QStringList mimeTypes;
+    PackageHandler::Node* node = m_packageHandler.loadPackageInfo();
+    for (auto it : node->childNodes()) {
+        mimeTypes.append(it->mimeTypes());
+        if (!it->childNodes().isEmpty()) {
+            for(auto childIt : it->childNodes()) {
+                mimeTypes.append(childIt->mimeTypes());
             }
         }
     }
-    Q_ASSERT(loopOk);
-    Q_ASSERT_X(indexedTopLevel.contains("bar.qml"),
-               "Checking top level unnamed files",
-               "bar.qml doesn't exist");
+    Q_ASSERT_X(mimeTypes.contains(QStringLiteral("[plasmate]/kconfigxteditor/")),
+               "Checking mimetypes","[plasmate]/kconfigxteditor/ doesn't exist");
+    Q_ASSERT_X(mimeTypes.contains(QStringLiteral("text/x-qml")), "Checking mimetypes",
+              "text/x-qml doesn't exist");
+    Q_ASSERT_X(mimeTypes.contains(QStringLiteral("[plasmate]/new")), "Checking mimetypes",
+              "text/x-qml doesn't exist");
+
 }
 
-void PackageHandlerTest::cleanupTestCase()
+void PackageHandlerTest::checkNodes()
 {
+    PackageHandler::Node *topNode = m_packageHandler.loadPackageInfo();
+    Q_ASSERT(topNode->name().isEmpty());
+    Q_ASSERT(topNode->description().isEmpty());
+    Q_ASSERT(topNode->mimeTypes().isEmpty());
+    Q_ASSERT(!topNode->parent());
+    Q_ASSERT(!topNode->childNodes().isEmpty());
+    for (auto it : topNode->childNodes()) {
+        QCOMPARE(topNode, it->parent());
+        Q_ASSERT(!it->name().isEmpty());
+        Q_ASSERT(!it->description().isEmpty());
+        if (!it->isFile()) {
+            const QString firstNodeDescription = it->childNodes().at(0)->description();
+            QCOMPARE(firstNodeDescription, QStringLiteral("New.."));
+        }
+    }
 }
+
+void PackageHandlerTest::checkPlasmoidNodes()
+{
+    PackageHandler::Node *topNode = m_packageHandler.loadPackageInfo();
+
+    QStringList topNodeNames;
+    QStringList childNodeNames;
+
+    auto populateChildNodeNamesList = [=](PackageHandler::Node *node) mutable {
+        childNodeNames.clear();
+        for (const auto it : node->childNodes()) {
+            childNodeNames.append(it->name());
+        }
+    };
+
+    Q_ASSERT_X(topNode->childNodes().size() == 9, "Check node list size", QString(node->childNodes().size()).toLocal8Bit().data());
+
+    for (const auto it : topNode->childNodes()) {
+        childNodeNames.clear();
+
+        if (it->description() == QStringLiteral("User Interface")) {
+            populateChildNodeNamesList(it);
+            Q_ASSERT(childNodeNames.size() == 2);
+            Q_ASSERT(childNodeNames.contains(QStringLiteral("New..")));
+            Q_ASSERT(childNodeNames.contains(QStringLiteral("Main Script File")));
+        } else if (it->description() == QStringLiteral("Configuration Definitions")) {
+            populateChildNodeNamesList(it);
+            Q_ASSERT(childNodeNames.size() == 3);
+            Q_ASSERT(childNodeNames.contains(QStringLiteral("New..")));
+            Q_ASSERT(childNodeNames.contains(QStringLiteral("Configuration UI Pages Model")));
+            Q_ASSERT(childNodeNames.contains("Configuration XML file"));
+        } else if (it->description() == QStringLiteral("Data Files") ||
+                   it->description() == QStringLiteral("Executable Scripts") ||
+                   it->description() == QStringLiteral("Images") ||
+                   it->description() == QStringLiteral("Translations") ||
+                   it->description() == QStringLiteral("Themed Images")) {
+            populateChildNodeNamesList(it);
+            Q_ASSERT(childNodeNames.size() == 1);
+            Q_ASSERT(childNodeNames.contains(QStringLiteral("New..")));
+        } else if (it->description() == QStringLiteral("metadata.desktop")) {
+            Q_ASSERT(childNodeNames.size() == 0);
+        } else if (it->description() == QStringLiteral("bar.qml")) {
+            populateChildNodeNamesList(it);
+            Q_ASSERT(childNodeNames.size() == 0);
+        } else {
+            const QString errorMessage = QString(QStringLiteral("Unrecognized name %1")).arg(it->name());
+            QFAIL(errorMessage.toLocal8Bit().data());
+        }
+    }
+
+    QStringList expectedTopLevelNodeNames;
+    expectedTopLevelNodeNames.append(QStringLiteral("User Interface"));
+    expectedTopLevelNodeNames.append(QStringLiteral("Configuration Definitions"));
+    expectedTopLevelNodeNames.append(QStringLiteral("Data Files"));
+    expectedTopLevelNodeNames.append(QStringLiteral("Executable Scripts"));
+    expectedTopLevelNodeNames.append(QStringLiteral("Images"));
+    expectedTopLevelNodeNames.append(QStringLiteral("Translations"));
+    expectedTopLevelNodeNames.append(QStringLiteral("Themed Images"));
+    expectedTopLevelNodeNames.append(QStringLiteral("metadata.desktop"));
+    Q_ASSERT(topNodeNames == expectedTopLevelNodeNames);
+}
+
 
 QTEST_GUILESS_MAIN(PackageHandlerTest)
 
--- plasmate/tests/packagehandlertest.h
+++ plasmate/tests/packagehandlertest.h
@@ -1,6 +1,6 @@
 /*
 
-   Copyright 2013 Giorgos Tsiapaliokas <terietor@gmail.com>
+Copyright 2013 Giorgos Tsiapaliokas <terietor@gmail.com>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
@@ -35,9 +35,12 @@ public:
 
 private Q_SLOTS:
     void initTestCase();
-    void checkDirectories();
-    void checkLoadPackageInfo();
-    void cleanupTestCase();
+    void initPlasmoid();
+    void createNewPlasmoid();
+    void loadPlasmoidTestData();
+    void checkPlasmoidMimeTypes();
+    void checkNodes();
+    void checkPlasmoidNodes();
 private:
     PackageHandler m_packageHandler;
 };
