5c1edc0bf51ebaee9ddf4af12515227e14303ad2
Antonis Tsiapaliokas <antonis.tsiapaliokas@kde.org>
2015-05-08 19:59:03 +0300

    Fix typo in Messages.sh

--- plasmoidviewer/qmlpackages/shell/Messages.sh
+++ plasmoidviewer/qmlpackages/shell/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_shell_org.kde.desktop.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_shell_org.kde.plasmoidviewershell.pot
 rm -f rc.cpp
