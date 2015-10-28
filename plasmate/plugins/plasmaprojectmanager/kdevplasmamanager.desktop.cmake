62af97bf43606651c6fe619e8ce1f302d93212bd
l10n daemon script <scripty@kde.org>
2015-10-28 12:41:14 +0000

    SVN_SILENT made messages (.desktop file)

--- plasmate/plugins/plasmaprojectmanager/kdevplasmamanager.desktop.cmake
+++ plasmate/plugins/plasmaprojectmanager/kdevplasmamanager.desktop.cmake
@@ -1,8 +1,17 @@
 [Desktop Entry]
 Type=Service
 Name=Plasma Project Manager
+Name[sk]=Správca projektov Plasma
+Name[sv]=Plasma-projekthanterare
+Name[x-test]=xxPlasma Project Managerxx
 GenericName=Plasma Project Manager
+GenericName[sk]=Správca projektov Plasma
+GenericName[sv]=Plasma-projekthanterare
+GenericName[x-test]=xxPlasma Project Managerxx
 Comment=Allow KDevPlatform to manage plasma projects
+Comment[sk]=Umožniť KDevPlatform spracovať projekty plasma
+Comment[sv]=Tillåter att KDevelop hanterar Plasma-projekt
+Comment[x-test]=xxAllow KDevPlatform to manage plasma projectsxx
 Icon=plasmagik
 ServiceTypes=KDevelop/Plugin
 X-KDE-Library=kdevplatform/@KDEV_PLUGIN_VERSION@/kdevplasmamanager
--- plasmate/plugins/savesystemview/kdevsavesystemview.desktop.cmake
+++ plasmate/plugins/savesystemview/kdevsavesystemview.desktop.cmake
@@ -2,8 +2,17 @@
 Type=Service
 Exec=blubb
 Comment=Lets you manage your git repository.
+Comment[sk]=Nechá vás spravovať váš git repozitár.
+Comment[sv]=Låter dig hantera Git-arkiv.
+Comment[x-test]=xxLets you manage your git repository.xx
 Name=Save System View
+Name[sk]=Uložiť systémový pohľad
+Name[sv]=Spara systemvy
+Name[x-test]=xxSave System Viewxx
 GenericName=Save System View
+GenericName[sk]=Uložiť systémový pohľad
+GenericName[sv]=Spara systemvy
+GenericName[x-test]=xxSave System Viewxx
 Icon=plasmagik
 ServiceTypes=KDevelop/Plugin
 X-KDE-Library=kdevsavesystemview
