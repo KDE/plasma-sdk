0176ffaa9d136d77349567926235ac435503600b
l10n daemon script <scripty@kde.org>
2015-12-06 09:18:13 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- plasmate/plugins/savesystemview/kdevsavesystemview.desktop.cmake
+++ plasmate/plugins/savesystemview/kdevsavesystemview.desktop.cmake
@@ -2,6 +2,7 @@
 Type=Service
 Exec=blubb
 Comment=Lets you manage your git repository.
+Comment[ast]=Déxate xestionar el to repositoriu git.
 Comment[ca]=Permet gestionar el vostre repositori git.
 Comment[ca@valencia]=Permet gestionar el vostre repositori git.
 Comment[de]=Verwaltung von Git-Quelltextarchiven.
