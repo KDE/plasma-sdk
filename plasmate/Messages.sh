e0393c4635afb8f06851bbb01d94d2acee48a04b
Jonathan Riddell <jr@jriddell.org>
2017-05-23 13:50:53 +0100

    FEATURE: don't build plasmate, it is unmaintained, moved to kde:plasmate

--- plasmate/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find -name \*.ui -o -name \*.rc | grep -v '/test-kpart/'` >> rc.cpp || exit 11
-$XGETTEXT `find -name \*.cpp -o -name \*.h | grep -v '/test/' | grep -v '/test-kpart/'` -o $podir/plasmate.pot
-rm -f rc.cpp
