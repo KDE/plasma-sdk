e0827fac9a9edad7831ce719b92f03e50f44f29d
David Edmundson <kde@davidedmundson.co.uk>
2023-10-05 22:20:12 +0100

    kqml: Add hot reload support
    When working on testing QML snippets, having to save and close and
    reopen is a drag.
    This patch adds support to reload any QML when the file on disk changes,
    making it suitable
    for rapid prototyping and testing
    
    As it's a relatively new dev tool, this is on by default. We can make it
    an option if it proves
    annoying.  A behavioural change this introduces is that invalid QML will
    open a window
    instead of quitting.

--- kqml/main.cpp
+++ kqml/main.cpp
@@ -1,27 +1,32 @@
 /*
-    SPDX-FileCopyrightText: 2023 ivan tkachenko <me@ratijas.tk>
+ SPDX-FileCopyrightText: 2023 ivan tkachenko <me@ratijas.tk>
+ SPDX-FileCopyrightText: 2023 David Edmundson <davidedmundson@kde.org>
 
-    SPDX-License-Identifier: GPL-2.0-or-later
- */
+ SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
-#include "KF6/PlasmaQuick/plasmaquick/sharedqmlengine.h"
 #include <QApplication>
 #include <QCommandLineOption>
 #include <QCommandLineParser>
 #include <QCoreApplication>
 #include <QDir>
-#include <QQmlApplicationEngine>
+#include <QQmlAbstractUrlInterceptor>
 #include <QQmlComponent>
 #include <QQmlContext>
+#include <QQmlEngine>
 #include <QQuickItem>
-#include <QQuickWindow>
+#include <QQuickView>
+
 #include <QScopedPointer>
 #include <QStringList>
+#include <QTimer>
 
+#include <KDirWatch>
 #include <KLocalizedContext>
 #include <KLocalizedString>
 
 #include <PlasmaQuick/QuickViewSharedEngine>
+#include <PlasmaQuick/SharedQmlEngine>
 
 #include <stdio.h>
 
@@ -31,21 +36,21 @@ void noFilesGiven()
     exit(1);
 }
 
-void contain(QQuickWindow *window, QQuickItem *item)
+class QmlFileChangeWatcher : public QObject, public QQmlAbstractUrlInterceptor
 {
-    if (!item) {
-        return;
-    }
+    Q_OBJECT
+public:
+    QmlFileChangeWatcher(std::shared_ptr<QQmlEngine>, QObject *parent);
+    QUrl intercept(const QUrl &path, DataType type) override;
 
-    auto contentItem = window->contentItem();
+Q_SIGNALS:
+    void dirty();
 
-    window->setWidth(item->width());
-    window->setHeight(item->height());
-    item->setParentItem(contentItem);
-    item->bindableWidth().setBinding(contentItem->bindableWidth().makeBinding());
-    item->bindableHeight().setBinding(contentItem->bindableHeight().makeBinding());
-    window->setVisible(true);
-}
+private:
+    void init();
+    std::unique_ptr<KDirWatch> m_fileWatcher;
+    QTimer m_resetTimer;
+};
 
 int main(int argc, char *argv[])
 {
@@ -97,16 +102,85 @@ int main(int argc, char *argv[])
         noFilesGiven();
     }
 
+    auto engine = PlasmaQuick::SharedQmlEngine().engine();
+    auto watcher = new QmlFileChangeWatcher(engine, &app);
+    QObject::connect(watcher, &QmlFileChangeWatcher::dirty, [&engine, files]() {
+        engine->clearComponentCache();
+        // engine->clearSingletons();
+    });
+
     // Load files
     for (const QString &path : std::as_const(files)) {
         QUrl url = QUrl::fromUserInput(path, QDir::currentPath(), QUrl::AssumeLocalFile);
         auto window = new PlasmaQuick::QuickViewSharedEngine();
+        window->setTranslationDomain(applicationDomain);
         window->setTitle(url.fileName());
         window->setSource(url);
         window->show();
+
+        QObject::connect(watcher, &QmlFileChangeWatcher::dirty, window, [window]() {
+            window->setSource(QUrl());
+            delete window->rootObject();
+        });
+
+        QObject::connect(
+            watcher,
+            &QmlFileChangeWatcher::dirty,
+            window,
+            [window, url]() {
+                qDebug() << "Reloading";
+                qDebug() << "-------------------";
+                window->setSource(url);
+            },
+            Qt::QueuedConnection);
+
+        QObject::connect(window, &PlasmaQuick::QuickViewSharedEngine::statusChanged, window, [window](QQmlComponent::Status status) {
+            if (status == QQmlComponent::Error) {
+                qWarning() << "Error loading file";
+            }
+        });
+        // status changed
     }
 
     return app.exec();
+}
+
+QmlFileChangeWatcher::QmlFileChangeWatcher(std::shared_ptr<QQmlEngine> engine, QObject *parent)
+    : QObject(parent)
+{
+    engine->addUrlInterceptor(this);
+    m_resetTimer.setInterval(1000);
+    m_resetTimer.setSingleShot(true);
+    connect(&m_resetTimer, &QTimer::timeout, this, [this, engine]() {
+        // reset watched files
+        init();
+        Q_EMIT dirty();
+    });
+    init();
+}
+
+void QmlFileChangeWatcher::init()
+{
+    m_fileWatcher.reset(new KDirWatch(this));
+    connect(m_fileWatcher.get(), &KDirWatch::dirty, this, [this]() {
+        if (!m_resetTimer.isActive()) {
+            m_resetTimer.start();
+        }
+    });
+}
+
+QUrl QmlFileChangeWatcher::intercept(const QUrl &path, DataType type)
+{
+    switch (type) {
+    case QmlFile:
+    case JavaScriptFile:
+    case QmldirFile:
+        m_fileWatcher->addFile(path.toLocalFile());
+        break;
+    default:
+        break;
+    }
+    return path;
 }
 
 #include "main.moc"
